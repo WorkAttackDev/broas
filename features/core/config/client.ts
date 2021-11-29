84a643c8bb2930a063005ea863730ffb26c59ac8
WorkAttack <workattackgeral@gmail.com>
2021-11-29 02:21:41 +0100

    add prod

--- features/core/config/client.ts
+++ features/core/config/client.ts
@@ -1,8 +1,12 @@
 import { Broas } from ".prisma/client";
 import axios, { AxiosResponse } from "axios";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: isProd
+    ? "https://broas.vercel.app/api"
+    : "http://localhost:3000/api",
 });
 
 export const getBroasClient = async () => {
